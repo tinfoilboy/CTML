3a1f1cc40629dea50e65b21e3688277e862e5280
Maxwell Flynn <tinfoilboy@gmail.com>
2015-10-27 23:13:25 -0400

    changed CTML into a header-only library. due to it only being two files.

--- CTML/Document.h
+++ CTML/Document.h
@@ -21,16 +21,62 @@ namespace CTML {
 		Node m_body;
 	public:
 		// the default constructor for a document
-		Document();
+		Document() {
+			// create and set the doctype to html
+			this->m_doctype = Node("", "html");
+			this->m_doctype.SetType(DOCUMENT_TYPE);
+			// create the head tag
+			this->m_head = Node("head");
+			// create the body tag
+			this->m_body = Node("body");
+		}
+
 		// add a node to the head element
-		void AddNodeToHead(Node node);
+		void AddNodeToHead(Node node) {
+			this->m_head.AppendChild(node);
+		}
+
 		// add a node to the body element
-		void AddNodeToBody(Node node);
+		void AddNodeToBody(Node node) {
+			this->m_body.AppendChild(node);
+		}
+
 		// gets the current document as a string
-		std::string ToString(bool readable);
+		std::string ToString(bool readable) {
+			std::string doc = "";
+			// add the doctype to the string
+			doc += m_doctype.ToString(readable, 0);
+			// every document needs an html tag, add it
+			doc += "<html>";
+			// if we want readability, append a newline to the html beginning tag
+			doc += ((readable) ? "\n" : "");
+			// append the head tag and its children
+			doc += m_head.ToString(readable, 1);
+			// append the body tag and its children
+			doc += m_body.ToString(readable, 1);
+			// close the html tag
+			doc += "</html>";
+			return doc;
+		}
+
 		// get the current document as a tree represented in a string
-		std::string ToTree();
+		std::string ToTree() {
+			std::string treeStr = "";
+			treeStr += "html\n";
+			treeStr += m_head.GetTreeString(0);
+			treeStr += m_body.GetTreeString(0);
+			return treeStr;
+		}
+
 		// write the current document to a file
-		bool WriteToFile(std::string filePath, bool readable);
+		bool WriteToFile(std::string filePath, bool readable) {
+			std::ofstream file = std::ofstream(filePath);
+			if (file.is_open()) {
+				file << this->ToString(readable);
+				file.close();
+				return true;
+			}
+			return false;
+		}
 	};
 }
--- CTML/Node.h
+++ CTML/Node.h
@@ -14,31 +14,182 @@ namespace CTML {
 	enum NodeType { DOCUMENT_TYPE, ELEMENT };
 
 	class Node {
+		// the type of node this
 		NodeType m_type;
+		// the name of this node, e.g. div, a, span, e.t.c.
 		std::string m_name;
+		// the classes for this node
 		std::string m_classes;
+		// the content of this node
 		std::string m_content;
+		// the child elements of this node
 		std::vector<Node> m_children;
 		// a map of attributes, name is the attribute name and the value is the attribute value
 		std::map<std::string, std::string> m_attributes;
 		// should new lines in m_content be represented with a br tag
-		bool useBrForNewLine = false;
+		bool m_newLineBR = false;
 	public:
 		Node() {}
+		
 		// create a node with the name specified
-		Node(std::string name);
-		Node(std::string name, std::string m_content);
-		std::string ToString(bool readable, int indentLevel);
-		std::string GetTreeString(int indentLevel);
-		Node& SetName(std::string name);
-		Node& SetAttribute(std::string name, std::string value);
-		Node& SetType(NodeType type);
-		Node& SetContent(std::string text);
-		Node& ToggleClass(std::string className);
-		Node& AppendChild(Node child);
-		Node& SetUseBr(bool useBr);
-		~Node();
+		Node(std::string name) {
+			this->m_type = ELEMENT;
+			this->SetName(name);
+		}
+
+		Node(std::string name, std::string m_content) {
+			this->m_type = ELEMENT;
+			this->SetName(name);
+			this->m_content = m_content;
+		}
+
+		std::string ToString(bool readable, int indentLevel) {
+			std::string elem = "";
+			// the four space indent.
+			std::string indent = "";
+			std::string indentContent = "";
+			// increment the indent string by four spaces based on the indentLevel
+			for (int i = 0; i < indentLevel; i++) {
+				indent += "    ";
+			}
+			// set the m_content indent level to the indent level plus four more spaces
+			indentContent = indent + "    ";
+			if (this->m_type == ELEMENT) {
+				// construct the first part of the element string, the tag beginning
+				elem = ((readable) ? indent : "") + "<" + m_name + "";
+				// add the class list if it isn't empty
+				if (!m_classes.empty()) {
+					std::string classTag = "class=\"";
+					elem += " " + classTag + m_classes + "\"";
+				}
+				// make an iterator for each attribute
+				for (auto attr : m_attributes) {
+					elem += " " + attr.first + "=\"" + attr.second + "\"";
+				}
+				// close the beginning tag
+				elem += ">";
+				// if readablity is wanted, add a newline
+				if (readable)
+					elem += "\n";
+				// if we have m_content to append
+				if (!m_content.empty()) {
+					// append the m_content to the node, if readability is wanted, add four spaces to the m_content and add a new line
+					if (!readable)
+						elem += m_content;
+					else
+						elem += this->_GetFormattedContent(indentContent);
+				}
+				// get every child node from the m_children list
+				for (unsigned int i = 0; i < m_children.size(); i++) {
+					Node childNode = m_children[i];
+					// append the child node to the elem string.
+					elem += childNode.ToString(readable, indentLevel + 1);
+				}
+				elem += ((readable) ? indent : "") + "</" + m_name + ">" + ((readable) ? "\n" : "");
+			}
+			else if (this->m_type == DOCUMENT_TYPE) {
+				// just construct the docm_type from the m_content given, if readability is wanted, add a newline
+				elem += "<!DOCTYPE " + m_content + ">" + ((readable) ? "\n" : "");
+			}
+			return elem;
+		}
+
+		std::string GetTreeString(int indentLevel) {
+			// the tree string
+			std::string tree = "";
+			// indent level
+			std::string indent = "";
+			// get the current indent string
+			for (int i = 0; i < indentLevel; i++) {
+				indent += "   ";
+			}
+			// turn the class list into actual classes for the elements
+			std::string classList = m_classes;
+			std::replace(classList.begin(), classList.end(), ' ', '.');
+			// if the class list isn't empty, prepend a period
+			if (!classList.empty())
+				classList = '.' + classList;
+			// add the current element to the tree
+			tree += indent + " |_ " + this->m_name + classList + '\n';
+			// for each child
+			for (unsigned int i = 0; i < m_children.size(); i++) {
+				tree += m_children[i].GetTreeString(indentLevel + 1) + '\n';
+			}
+			return tree;
+		}
+
+		Node& SetName(std::string name) {
+			// the index of a period
+			int periodIndex = name.find('.');
+			// if there are classes in the name
+			if (periodIndex != std::string::npos) {
+				// get the element name
+				std::string elemName = name.substr(0, periodIndex);
+				// get the classes for this element
+				std::string classes = name.substr(periodIndex + 1);
+				// replace each class period with a space
+				std::replace(classes.begin(), classes.end(), '.', ' ');
+				this->m_name = elemName;
+				this->m_classes = classes;
+			}
+			else {
+				this->m_name = name;
+			}
+			return *this;
+		}
+		Node& SetAttribute(std::string name, std::string value) {
+			m_attributes[name] = value;
+			return *this;
+		}
+
+		Node& SetType(NodeType type) {
+			this->m_type = type;
+			return *this;
+		}
+
+		Node& SetContent(std::string text) {
+			this->m_content = text;
+			return *this;
+		}
+
+		Node& ToggleClass(std::string className) {
+			int findIndex = m_classes.find(className);
+			if (findIndex == std::string::npos) {
+				// append the class
+				m_classes += ((!m_classes.empty()) ? " " : "") + className;
+			}
+			else {
+				// remove the class
+				m_classes.erase(findIndex, className.size());
+			}
+			return *this;
+		}
+		
+		Node& AppendChild(Node child) {
+			m_children.push_back(child);
+			return *this;
+		}
+
+		Node& SetUseBr(bool useBr) {
+			this->m_newLineBR = useBr;
+			return *this;
+		}
+
+		~Node() {
+			m_attributes.clear();
+			m_children.clear();
+		}
 	private:
-		std::string GetFormattedContent(std::string indent);
+		std::string _GetFormattedContent(std::string indent) {
+			std::string newline = ((this->m_newLineBR) ? "\n" + indent + "<br>\n" : "\n");
+			std::string result;
+			std::istringstream iss(m_content);
+			// iterate through each line in this node
+			for (std::string line; std::getline(iss, line);)
+			{
+				result += indent + line + newline;
+			}
+			return result;
+		}
 	};
 };
