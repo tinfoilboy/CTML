85e366c4829b346874c1679eeb7d4bcf2c3eddf6
Maxwell Flynn <tinfoilboy@gmail.com>
2016-05-04 19:29:42 -0400

    updated tests.cpp to have test completion time

--- CTML/tests.cpp
+++ CTML/tests.cpp
@@ -1,5 +1,8 @@
+#include <iostream>
 #include "CTML.h"
 
+
+using namespace CTML;
 // tests if two strings are equivelent to each other
 bool assert_strings_equal(const std::string& left, const std::string& right) {
 	return (left == right);
@@ -8,11 +11,11 @@ bool assert_strings_equal(const std::string& left, const std::string& right) {
 // this test ensures that HTML inside of the content is escaped correctly.
 void run_escape_test() {
 	// what the node's to string should be equal to
-	const std::string htmlString = "<a class=\"button\">&lt;script&gt;alert(\"ha ha hacked!\")&lt;/script&gt;</a>";
-	CTML::Node node("a.button", "<script>alert(\"ha ha hacked!\")</script>");
+	const auto htmlString = "<a class=\"button\">&lt;script&gt;alert(\"ha ha hacked!\")&lt;/script&gt;</a>";
+	Node node("a.button", "<script>alert(\"ha ha hacked!\")</script>");
 	// the node's string output
-	const std::string nodeString = node.ToString(CTML::Readability::SINGLE_LINE, 0);
-	bool test = assert_strings_equal(htmlString, nodeString);
+	const auto nodeString = node.ToString(Readability::SINGLE_LINE, 0);
+	auto test = assert_strings_equal(htmlString, nodeString);
 	std::cout << "Escape Test " << ((test) ? "passed!" : "failed!") << std::endl <<
 		"HTML Output: " << nodeString << std::endl <<
 		// use a double end line at the end for spacing between tests
@@ -22,12 +25,12 @@ void run_escape_test() {
 // this test ensures that the HTML document created is equal to a correct HTML5 document
 void run_document_test() {
 	// what the document's to string should be equal to
-	const std::string htmlString = "<!DOCTYPE html><html><head></head><body><h1>&lt;test!&gt;</h1></body></html>";
-	CTML::Document doc;
+	const auto htmlString = "<!DOCTYPE html><html><head></head><body><h1>&lt;test!&gt;</h1></body></html>";
+	Document doc;
 	// the string output of the document
-	doc.AddNodeToBody(CTML::Node("h1", "<test!>"));
-	const std::string docString = doc.ToString(CTML::Readability::SINGLE_LINE);
-	bool test = assert_strings_equal(htmlString, docString);
+	doc.AddNodeToBody(Node("h1", "<test!>"));
+	const auto docString = doc.ToString(Readability::SINGLE_LINE);
+	auto test = assert_strings_equal(htmlString, docString);
 	std::cout << "Document Test " << ((test) ? "passed!" : "failed!") << std::endl <<
 		"HTML Output: " << docString << std::endl <<
 		// use a double end line at the end for spacing between tests
@@ -36,10 +39,10 @@ void run_document_test() {
 
 // this test checks if the classes provided are correctly stored
 void run_class_test() {
-	const std::string classString = "test classes are fun";
-	CTML::Node testNode("a#test.test.classes.are.fun");
+	const auto classString = "test classes are fun";
+	Node testNode("a#test.test.classes.are.fun");
 	// get the test node's classlist.
-	const std::string classList = testNode.GetAttribute("class");
+	const auto classList = testNode.GetAttribute("class");
 	bool test = assert_strings_equal(classString, classList);
 	std::cout << "Class Test " << ((test) ? "passed!" : "failed!") << std::endl <<
 		"Class Output: " << classList << std::endl <<
@@ -49,20 +52,20 @@ void run_class_test() {
 
 // this test checks if the attributes provided are correctly stored and gotten
 void run_attribute_test() {
-	const std::string attrOutput = "testAttr1";
-	const std::string attr2Output = "testAttr2";
-	const std::string attr3Output = "";
-	CTML::Node testNode("a");
+	const auto attrOutput = "testAttr1";
+	const auto attr2Output = "testAttr2";
+	const auto attr3Output = "";
+	Node testNode("a");
 	// set two attributes on the node
 	testNode.SetAttribute("attr1", "testAttr1").SetAttribute("attr2", "testAttr2");
 	// get each attribute's output
-	const std::string attrOut = testNode.GetAttribute("attr1");
-	const std::string attr2Out = testNode.GetAttribute("attr2");
-	const std::string attr3Out = testNode.GetAttribute("attr3");
+	const auto attrOut = testNode.GetAttribute("attr1");
+	const auto attr2Out = testNode.GetAttribute("attr2");
+	const auto attr3Out = testNode.GetAttribute("attr3");
 	// test each string
-	bool test1 = assert_strings_equal(attrOutput, attrOut);
-	bool test2 = assert_strings_equal(attr2Output, attr2Out);
-	bool test3 = assert_strings_equal(attr3Output, attr3Out);
+	auto test1 = assert_strings_equal(attrOutput, attrOut);
+	auto test2 = assert_strings_equal(attr2Output, attr2Out);
+	auto test3 = assert_strings_equal(attr3Output, attr3Out);
 	std::cout << "Class Test " << ((test1 && test2 && test3) ? "passed!" : "failed!") << std::endl <<
 		"Attr 1 Output: " << attrOut << std::endl <<
 		"Expected Output: " << attrOutput << std::endl <<
@@ -74,10 +77,9 @@ void run_attribute_test() {
 
 }
 
-int main() {
+int main(int argc, char **argv) {
 	run_escape_test();
 	run_document_test();
 	run_class_test();
 	run_attribute_test();
-	return 0;
 }
